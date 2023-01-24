2ba97a55d98f497066469f6b7c4c8349014440f4
Pascale, Michael Philip <mppascale@mgh.harvard.edu>
2023-01-24 16:08:14 -0500

    feat(events.ts): introduce CalendarDate class, implement UseEvent, abstract CalendarEvent

--- src/events.ts
+++ src/events.ts
@@ -4,96 +4,225 @@
  * Authored by Michael Pascale <mppascale@mgh.harvard.edu>.
  * Last modified: 2023-01-24
  */
-// interface CalendarEvent {
-//     // Event ID
-//     // A unique identifier for the event.
-//     eid: number;
-//     // Group ID.
-//     // Events are grouped if they belong to the same recurrence.
-//     gid: number;
-//     // Event type.
-//     type: string;
-//     // Date of the event.
-//     date: Date;
-//     // Is the event recurring?
-//     recur: Boolean;
-// };
 
+type UseEventProperties = {
+    category: string;
+    substance: string;
+    times: number;
+    amount: number;
+    units: string;
+}
 
-// interface KeyEvent extends CalendarEvent {
-//     type: 'key';
-//     title: string;
-// }
-
-// interface UseEvent extends CalendarEvent {
-//     type: 'sub';
-//     title: string;
-// }
-
-type EventType = 'key' | 'use' | null;
-type EventIdentifier = number | null;
-
-
-class CalendarEvent {
-
-    private _eid: EventIdentifier = null;
-    private _gid: EventIdentifier = null;
+class CalendarDate {
     private _date: Date;
-    protected _type: EventType = null;
 
-    // Constructor iis protected such that a CalendarEvent can only be
-    // instantiated under a derived class.
-    protected constructor(date: Date, type: EventType) {
-        this._date = date;
+    // CalendarDate expects ISO 8601 'YYYY-MM-DD' date and interprets it in UTC.
+    public constructor(date: string) {
+
+        if (!(/^\d{4}-\d{2}-\d{2}$/).test(date))
+            throw new Error('CalendarDate expects a date in \'YYYY-MM-DD\' format.')
+
+        this._date = new Date(date);
+    }
+
+    public get Date(): Date {
+        return this._date;
+    }
+
+    // Weekday where Sunday == 1.
+    public get weekday(): number {
+        return this._date.getUTCDay() + 1;
+    }
+
+    // Day of the month.
+    public get day(): number {
+        return this._date.getUTCDate();
+    }
+
+    // Month where January == 1.
+    public get month(): number {
+        return this._date.getUTCMonth() + 1;
+    }
+
+    // Four digit year.
+    public get year(): number  {
+        return this._date.getUTCFullYear();
+    }
+
+    public toString(): string {
+        return this._date.toISOString().substring(0,10)
+    }
+
+    public set_day(day: number): CalendarDate {
+        this._date.setUTCDate(day);
+        return this;
+    }
+
+    public isSameDay(date: CalendarDate) {
+        return this.year == date.year && this.month == date.month && this.day == date.day;
+    }
+
+    public isAfter(date: CalendarDate): boolean {
+        return (this.year == date.year && this.month == date.month && this.day > date.day) ||
+            (this.year == date.year && this.month > date.month) ||
+            (this.year > date.year);
+    }
+
+    public isBefore(date: CalendarDate): boolean {
+        return (this.year == date.year && this.month == date.month && this.day < date.day) ||
+            (this.year == date.year && this.month < date.month) ||
+            (this.year < date.year);
+    }
+
+    public get next_day() {
+        return new CalendarDate(this.toString()).set_day(this.day + 1);
+    }
+}
+
+abstract class CalendarEvent {
+
+    private _eid = 0;
+    private _gid = 0;
+    private _date: CalendarDate;
+    protected readonly _type: 'key' | 'use';
+
+    public constructor(date: string, type: 'key' | 'use') {
+        this._date = new CalendarDate(date);
         this._type = type;
     }
 
-    public set_eid(eid: number) {
+    public abstract clone(): CalendarEvent;
+
+    public get eid() {
+        return this._eid;
+    }
+
+    public get gid() {
+        return this._gid;
+    }
+
+    public get date() {
+        return this._date.toString();
+    }
+
+    public set_eid(eid: number): CalendarEvent {
         this._eid = eid;
         return this;
     }
 
-    public set_gid(gid: number) {
+    public set_gid(gid: number): CalendarEvent {
         this._gid = gid;
         return this;
+    }
+
+    public set_date(date: string | CalendarDate): CalendarEvent {
+        if (date instanceof CalendarDate)
+            this._date = date;
+        else
+            this._date = new CalendarDate(date);
+        return this;
+    }
+
+    // Recurring events are represented as an array of events with the same gid.
+    // The until date for a recurring event is *inclusive*.
+    public make_recurrence(weekdays: Array<number>, until: string): Array<CalendarEvent> {
+
+        const events: Array<CalendarEvent> = [];
+
+        for (let dt = this._date; !dt.isAfter(new CalendarDate(until)); dt = dt.next_day) {
+
+            if (weekdays.includes(dt.weekday)) {
+                events.push(this.clone().set_gid(this.gid).set_eid(this.eid).set_date(dt));
+            }
+        }
+
+        return events;
     }
 }
 
 class KeyEvent extends CalendarEvent {
-    private _title: string;
+    private readonly _title: string;
 
-    public constructor(date: Date, title: string) {
+    public constructor(date: string, title: string) {
         super(date, 'key');
         this._title = title;
+    }
+
+    public clone(): CalendarEvent {
+        return new KeyEvent(this.date, this._title);
     }
 }
 
 class UseEvent extends CalendarEvent {
-    private _title: string;
+    private _title = 'Uninitialized UseEvent';
+    private _category = '';
+    private _substance = '';
+    private _times = 0;
+    private _amount = 0;
+    private _units = '';
 
-    public constructor(date: Date, title: string) {
-        super(date, 'key');
-        this._title = title;
+    public constructor(date: string, properties: UseEventProperties | null) {
+        super(date, 'use');
+
+        if (properties != null)
+            this.set_properties(properties);
+    }
+
+    public clone(): CalendarEvent {
+        return new UseEvent(this.date, this.properties);
+    }
+
+    public get title() {
+        return this._title;
+    }
+
+    public get properties(): UseEventProperties {
+        return {
+            category:  this._category,
+            substance: this._substance,
+            times:     this._times,
+            amount:    this._amount,
+            units:     this._units
+        };
+    }
+
+    public set_properties(properties: UseEventProperties): CalendarEvent {
+        this._category  = properties.category;
+        this._substance = properties.substance;
+        this._times     = properties.times;
+        this._amount    = properties.amount;
+        this._units     = properties.units;
+        this.update_title();
+
+        return this;
+    }
+
+    // Set the title for the event given the event properties.
+    private update_title() {
+        this._title = this._substance + ' ' + this._times + 'x ' + this._amount +  this._units;
     }
 }
 
 
 // A CalendarEvents object stores an array of calendar events and provides
 // a safe interface through which they can be accessed and updated.
-class CalendarEvents {
+class CalendarEventList {
     private _next_eid: number;
     private _next_gid: number;
     private _events: Array<CalendarEvent>;
-    public readonly start_date: Date;
-    public readonly end_date: Date;
+    public readonly start_date: CalendarDate;
+    public readonly end_date: CalendarDate;
 
     // Start and end dates are *inclusive *. 
-    constructor(start: Date, end: Date) {
+    public constructor(start: string, end: string, events: Array<CalendarEvent> | null = null) {
         this._next_eid = 1;
         this._next_gid = 1;
         this._events = [];
-        this.start_date = start;
-        this.end_date = end;
+        this.start_date = new CalendarDate(start);
+        this.end_date = new CalendarDate(end);
+
+        if (events != null)
+            this.import_events(events);
     }
 
     private _get_next_eid() {
@@ -108,40 +237,37 @@ class CalendarEvents {
         return next_gid;
     }
 
-    public get_events() {
+    public get_events(): Array<CalendarEvent> {
         return this._events;
     }
 
-    public add_event(type: 'key' | 'sub', date: Date) {
-        this._events.push({
-            eid: this._get_next_eid(),
-            gid: this._get_next_gid(),
-            type: type,
-            date: date,
-            recur: false
-        });
-
+    public add(event: CalendarEvent): CalendarEventList {
+        this.import_events([event]);
         return this;
     }
 
-    public delete_event(eid: number) {
+    public delete_event(eid: number): CalendarEventList {
         this._events = this._events.filter((event: CalendarEvent) => event.eid != eid);
         return this;
     }
 
-    public delete_group(gid: number) {
+    public delete_group(gid: number): CalendarEventList {
         this._events = this._events.filter((event: CalendarEvent) => event.gid != gid);
         return this;
     }
 
     // Import events from an array.
-    public import_events(events: Array<CalendarEvent>) {
+    public import_events(events: Array<CalendarEvent>): CalendarEventList {
         
-        // Group IDs must be preserved.
+        // Group relationships must be preserved.
         const gid_mapping = 
             events.reduce((acc: Map<number, number>, cur: CalendarEvent) => {
+                if (cur.gid == null)
+                  throw new Error('Event is missing a GID and cannot be imported.');
+
                 if (!acc.has(cur.gid))
                     acc.set(cur.gid, this._get_next_gid());
+                    
                 return acc;
             }, new Map());
 
@@ -149,10 +275,10 @@ class CalendarEvents {
         this._events = 
             this._events.concat(events.map((event: CalendarEvent) => {
                 // Events can be assigned a completely new eid.
-                event.eid = this._get_next_eid();
-                // @ts-ignore
-                // Map.prototype.get() can return undefined. That shouldn't happen here.
-                event.gid = gid_mapping.get(event.gid);
+                event.set_eid(this._get_next_eid());
+
+                // @ts-expect-error Map.prototype.get() can return undefined. That shouldn't happen here.
+                event.set_gid(gid_mapping.get(event.gid));
                 return event;
             }));
         
@@ -169,9 +295,5 @@ class CalendarEvents {
                 alert('CalendarEvents.serialize_events(\'json\'): Not implemented.')
                 break;
         }
-    };
-};
-
-
-// TODO: Can you create a runtime "checker" in TypeScript to validate foreign
-// objects such as those uploaded in a file?
+    }
+}
